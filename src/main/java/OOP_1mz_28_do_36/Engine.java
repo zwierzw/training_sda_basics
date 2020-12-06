aae55d3f16aefa0792a48ccac4fbe702c27dd51c
Wojtek <zwierzw@wp.pl>
2020-12-06 11:52:00 +0100

    Created Engine

---
+++ src/main/java/OOP_1mz_28_do_36/Engine.java
@@ -0,0 +1,36 @@
+package OOP_1mz_28_do_36;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// Ex28
+public class Engine {
+    private Double capacity;
+    @Getter
+    @Setter
+    private Integer horsePower;
+    @Getter
+    @Setter
+    private Double fuelConsumption;
+
+    //Ex29
+    public Engine (Double capacity, Integer horsePower, Double fuelConsumption) {
+        this.capacity = capacity;
+        this.horsePower = horsePower;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    //Ex30&31
+    public void setCapacity (Double capacity) {
+        this.capacity = capacity;
+    }
+
+    public Double getCapacity () {
+        return this.capacity;
+    }
+
+    //Ex32
+    
+
+
+}
