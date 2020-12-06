7d476ab3374befa394475ac919f68cf50118aa0d
Wojtek <zwierzw@wp.pl>
2020-12-06 09:55:48 +0100

    Dodanie maina oraz modyfikacja pom.xml

---
+++ src/main/java/collections_training/Main.java
@@ -0,0 +1,7 @@
+package collections_training;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/collections_training/test_class_inside_git.java
@@ -0,0 +1,7 @@
+package collections_training;
+
+public class test_class_inside_git {
+    public static void main (String[] args){
+        System.out.println("It's a test");
+    }
+}
