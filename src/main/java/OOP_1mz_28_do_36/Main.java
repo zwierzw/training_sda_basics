ca150d76ac91402bb95c66a478edd2b814212c51
Wojtek <zwierzw@wp.pl>
2020-12-06 12:15:44 +0100

    Utworzenie w Mainie SportCar

---
+++ src/main/java/OOP_1mz_28_do_36/Main.java
@@ -0,0 +1,8 @@
+package OOP_1mz_28_do_36;
+
+public class Main {
+    public static void main(String[] args) {
+        Engine engine = new Engine(12.02d,1200, 15.23);
+        SportCar sportCar = new SportCar("Opel", "Corsa", "red", 2, engine);
+    }
+}
