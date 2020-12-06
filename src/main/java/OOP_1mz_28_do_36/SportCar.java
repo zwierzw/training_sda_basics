bdf998f470c141b71d74dfb5103c868893434801
Wojtek <zwierzw@wp.pl>
2020-12-06 12:11:03 +0100

    SportCar dziedziczy po Car

---
+++ src/main/java/OOP_1mz_28_do_36/SportCar.java
@@ -0,0 +1,8 @@
+package OOP_1mz_28_do_36;
+
+public class SportCar extends Car {
+
+    public SportCar (String producer, String model, String color, Integer seatsNumber, Engine engine) {
+        super(producer, model, color, seatsNumber, engine);
+    }
+}
